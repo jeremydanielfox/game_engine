583ccb4dada17c793769e95382928da102659843
Brandon Choi <jc440@duke.edu>
2015-03-31 20:36:29 -0400

    next game view after initial view set up

--- src/gae/openingView/DataForm.java
+++ src/gae/openingView/DataForm.java
@@ -1,5 +1,6 @@
-package openingView;
+package gae.openingView;
 
+import gae.gameView.GameView;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -9,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class DataForm {
 
@@ -18,9 +20,13 @@ public class DataForm {
 
     private VBox form;
     private VBox inputFields;
+    
+    private Stage myStage;
 
-    public DataForm () {
+    public DataForm (Stage stage) {
         initialize();
+        
+        myStage = stage;
     }
 
     public Node getForm () {
@@ -28,7 +34,7 @@ public class DataForm {
     }
 
     private void initialize () {
-        form = new VBox(20);
+        form = new VBox(30);
         form.setId("dataForm");
         HEADER.setId("welcomeBanner");
         inputFields = new VBox(10);
@@ -54,7 +60,11 @@ public class DataForm {
         box.setPrefWidth(600);
         Button b = new Button(BUTTON_TEXT);
         b.setId("authorButton");
-        b.setOnMouseClicked(e -> System.out.println("clicked"));
+        b.setOnMouseClicked(e -> {
+            GameView author = new GameView();
+            //best way to pass around stage?
+            myStage.setScene(author.getScene());
+        });
         box.setAlignment(Pos.BASELINE_RIGHT);
         box.getChildren().add(b);
         return box;
--- src/gae/openingView/ImagePanel.java
+++ src/gae/openingView/ImagePanel.java
@@ -1,7 +1,9 @@
-package openingView;
+package gae.openingView;
 
+import gae.gameView.Main;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -13,13 +15,13 @@ public class ImagePanel {
     private static final ImageView SIDE_VIEW_GAME = new ImageView("/images/cartoonwars.jpg");
     private static final Text SET_PATH = new Text("Set Path");
     private static final Text FREE_PATH = new Text("Free Path");
-    private static final Text SIDE_VIEW = new Text("Side View / Runner");
-    private static final double IMAGE_WIDTH = OpeningView.SCREEN.getVisualBounds().getWidth() / 4;
-    private static final double IMAGE_HEIGHT = OpeningView.SCREEN.getVisualBounds().getHeight() / 4;
+    private static final Text SIDE_VIEW = new Text("Side View");
+    private static final double IMAGE_WIDTH = Main.SCREEN_WIDTH / 4;
+    private static final double IMAGE_HEIGHT = Main.SCREEN_HEIGHT / 4;
     private static final Text HEADER = new Text("Select Tower Defense Genre: ");
 
     private VBox panel;
-    private VBox images;
+    private VBox selections;
     private HoverPicture setPath, freePath, sideView;
 
     public ImagePanel () {
@@ -32,21 +34,22 @@ public class ImagePanel {
 
     private void initialize () {
         panel = new VBox(15);
-        images = new VBox(15);
+        selections = new VBox(15);
         HEADER.setId("selectGameText");
         panel.setId("rightPanel");
-        images.setId("imagePanel");
-        
+        selections.setId("imagePanel");
+
         setPath = new HoverPicture(SET_PATH_GAME, SET_PATH);
         freePath = new HoverPicture(FREE_PATH_GAME, FREE_PATH);
         sideView = new HoverPicture(SIDE_VIEW_GAME, SIDE_VIEW);
-        
-        images.getChildren().addAll(setPath.getView(), freePath.getView(), sideView.getView());
-        setUpClick(images);
-        panel.getChildren().addAll(HEADER, images);
+
+        selections.getChildren().addAll(setPath.getView(), freePath.getView(), sideView.getView());
+        setUpClick(selections);
+        panel.getChildren().addAll(HEADER, selections);
     }
 
-    private void setUpClick (Node choices) {
+    private void setUpClick (Pane choices) {
+        //if one is selected, disable the rest
         
     }
 
@@ -55,6 +58,7 @@ public class ImagePanel {
         private StackPane view;
         private ImageView image;
         private Text label;
+        private boolean selected;
 
         public HoverPicture (ImageView i, Text subtext) {
             view = new StackPane();
@@ -63,21 +67,43 @@ public class ImagePanel {
             i.setFitHeight(IMAGE_HEIGHT);
             i.setFitWidth(IMAGE_WIDTH);
             label = subtext;
+            label.getStyleClass().add("gameLabel");
             view.getChildren().addAll(i, label);
             setHoverEffect();
+            selected = false;
+            setSelectEffect();
+        }
+
+        public Node getView () {
+            return view;
         }
         
-        public Node getView() {
-            return view;
+        public boolean selectStatus () {
+            return selected;
         }
 
         private void setHoverEffect () {
-            image.setOnMouseEntered(e -> {
-                label.setVisible(false);
-            });
-            image.setOnMouseExited(e -> {
-                label.setVisible(true);
-            });
+            if (!selected) {
+                image.setOnMouseEntered(e -> {
+                    label.setVisible(false);
+                });
+                image.setOnMouseExited(e -> {
+                    label.setVisible(true);
+                });
+            }
+        }
+
+        private void setSelectEffect () {
+            if (selected) {
+                view.setOnMouseClicked(e -> {
+                    selected = false;
+                });
+            }
+            else {
+                view.setOnMouseClicked(e -> {
+                    selected = true;
+                });
+            }
         }
     }
 }

d6fbc36f2713351f2bd210a22eee3512a0ad0af4
Thomas Puglisi <tom.puglisi@gmail.com>
2015-04-01 21:40:25 -0400

    Added some methods to Shop to manage the prototype map

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -1,6 +1,7 @@
 package shop;
 
 import gameobject.GameObject;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,20 +10,44 @@ import shop.upgrade.Upgrade;
 
 public class Shop {
     private Map<ItemGraphic, Purchasable> purchasableMap;
+    private Map<Purchasable, ItemGraphic> itemGraphicMap;
 
     public Shop (List<Purchasable> purchasables) {
-        populatePurchasableMap(purchasables);
+        populateMaps(purchasables);
     }
 
-    private void populatePurchasableMap (List<Purchasable> purchasables) {
+    /**
+     * 
+     * @param purchasables list of purchasable items to be added to the bidirectional map
+     */
+    private void populateMaps (List<Purchasable> purchasables) {
         purchasableMap = new HashMap<ItemGraphic, Purchasable>();
+        itemGraphicMap = new HashMap<Purchasable, ItemGraphic>();
         purchasables.stream().forEach(purchasable -> purchasableMap
-                                              .put(new ItemGraphic("tempIcon", "tempTower"),
-                                                   purchasable));
+                .put(new ItemGraphic("tempIcon", "tempTower"),
+                     purchasable));
+        purchasableMap.keySet().stream()
+                .forEach(itemGraphic -> purchasables.stream().forEach(purchasable -> itemGraphicMap
+                        .put(purchasable, itemGraphic)));
     }
 
-    public List<Purchasable> getPurchasables (GameObject gameObject) {
-        return null;
+    public List<ItemGraphic> getItemGraphics (GameObject gameObject) {
+        List<ItemGraphic> items = new ArrayList<>();
+        itemGraphicMap.keySet().stream().forEach(purchasable -> addPurchasable(gameObject, items, purchasable));
+        return items;
+    }
+    
+    private void addPurchasable (GameObject gameObject, List<ItemGraphic> items, Purchasable purchasable) {
+        if (gameObject.canPurchase(purchasable)) {
+            items.add(itemGraphicMap.get(purchasable));
+        }
     }
 
+    public Purchasable getPurchasable(ItemGraphic itemGraphic) {
+        return purchasableMap.get(itemGraphic);
+    }
+    
+    public ItemGraphic getItemGraphic(Purchasable purchasable) {
+        return itemGraphicMap.get(purchasable);
+    }
 }

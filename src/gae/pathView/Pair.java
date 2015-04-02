6de9c8dd29134b6bee15e9f6b84a27e8cc00d4b0
keikumata <ky52@duke.edu>
2015-04-01 22:40:11 -0400

    made pathView package, path and snap-in works

---
+++ src/gae/pathView/Pair.java
@@ -0,0 +1,10 @@
+package gae.pathView;
+
+public class Pair {
+    double x, y;
+
+    public Pair (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/gae/pathView/Path.java
@@ -0,0 +1,15 @@
+package gae.pathView;
+
+public class Path {
+    private Pair start;
+    private Pair end;
+    private Pair control1;
+    private Pair control2;
+
+    public Path (Pair start, Pair end, Pair control1, Pair control2) {
+        this.start = start;
+        this.end = end;
+        this.control1 = control1;
+        this.control2 = control2;
+    }
+}

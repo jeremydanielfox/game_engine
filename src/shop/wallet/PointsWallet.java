0636d5a4eb46513f542c08c621c8cf45b46c2685
Nathan Prabhu <nprab428@yahoo.com>
2015-04-02 03:04:06 -0400

    create new wallet hierarchy, possible solution to currency #31

---
+++ src/shop/wallet/PointsWallet.java
@@ -0,0 +1,23 @@
+package shop.wallet;
+
+public class PointsWallet extends Wallet {
+
+    @Override
+    double getBalance () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    void deposit () {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    void withdraw () {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/shop/wallet/Wallet.java
@@ -0,0 +1,10 @@
+package shop.wallet;
+
+public abstract class Wallet {
+
+    abstract double getBalance();
+    
+    abstract void deposit();
+    
+    abstract void withdraw();
+}

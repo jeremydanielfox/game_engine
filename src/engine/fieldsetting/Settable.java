d338f1557f3f99b5501ca040bcba2a30bc37d0e5
Jeremy Fox <jeremy.daniel.fox@gmail.com>
2015-04-04 16:09:31 -0400

    Create settable annotation

---
+++ src/engine/fieldsetting/Settable.java
@@ -0,0 +1,13 @@
+package engine.fieldsetting;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+/**
+ * 
+ * @author Jeremy
+ *
+ */
+@Target(ElementType.METHOD)
+public @interface Settable {
+
+}

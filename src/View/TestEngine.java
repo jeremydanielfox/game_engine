c76cef27a51b974b001e17bdbb9432c012d26ff1
Sierra Smith <sms95@duke.edu>
2015-04-04 14:16:04 -0400

    Created another view with a border pane layout and updated HUD layout.

--- src/View/TestEngine.java
+++ src/View/TestEngine.java
@@ -13,19 +13,46 @@ import gae.gameView.Main;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class TestEngine extends Application {
 
+//    @Override
+//    public void start (Stage primaryStage) throws Exception {
+//        ConcreteLevelBoard board=new ConcreteLevelBoard();
+//        Group root=new Group();
+//        primaryStage.setHeight(Main.SCREEN_HEIGHT); // needs to account for scaling; add constants
+//        primaryStage.setWidth(Main.SCREEN_WIDTH);// needs to account for scaling; add constants
+//        Scene scene=new Scene(root);
+//        Player myPlayer=new Player("Boi",100,100,100);
+//        View view = new ConcreteView(new ConcreteGame(myPlayer,board),root,Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+//        HealthDepletionGoal healthy=new HealthDepletionGoal(myPlayer);
+//        ScoreGoal score=new ScoreGoal(myPlayer,200);
+//        List<Goal> list=new ArrayList<Goal>();
+//        list.add(healthy);
+//        List<Goal> list2=new ArrayList<Goal>();
+//        list2.add(score);
+//        List<Goal> list3=new ArrayList<Goal>();
+//        ScoreGoal score2=new ScoreGoal(myPlayer,300);
+//        list3.add(score2);
+//        board.addLevel(new ConcreteLevel("images/Park_Path.png",list2,list));
+//        board.addLevel(new ConcreteLevel("images/example_path.jpeg",list3,list));
+//        view.initializeGameWorld();
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+    
     @Override
     public void start (Stage primaryStage) throws Exception {
         ConcreteLevelBoard board=new ConcreteLevelBoard();
         Group root=new Group();
         primaryStage.setHeight(Main.SCREEN_HEIGHT); // needs to account for scaling; add constants
         primaryStage.setWidth(Main.SCREEN_WIDTH);// needs to account for scaling; add constants
+        //BorderPane pane = new BorderPane();
         Scene scene=new Scene(root);
         Player myPlayer=new Player("Boi",100,100,100);
-        View view = new ConcreteView(new ConcreteGame(myPlayer,board),root,Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+        View view = new ViewConcrete2(new ConcreteGame(myPlayer,board),Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
         HealthDepletionGoal healthy=new HealthDepletionGoal(myPlayer);
         ScoreGoal score=new ScoreGoal(myPlayer,200);
         List<Goal> list=new ArrayList<Goal>();
@@ -37,7 +64,8 @@ public class TestEngine extends Application {
         list3.add(score2);
         board.addLevel(new ConcreteLevel("images/Park_Path.png",list2,list));
         board.addLevel(new ConcreteLevel("images/example_path.jpeg",list3,list));
-        view.initializeGameWorld();
+        //pane.setCenter(view.initializeView());
+        root.getChildren().add(view.initializeView());
         primaryStage.setScene(scene);
         primaryStage.show();
     }

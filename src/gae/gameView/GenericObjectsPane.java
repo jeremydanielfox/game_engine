052e299f88405541fbf786ec703a5dd6c211fb11
jgilhuly <jrg37@duke.edu>
2015-04-08 22:23:32 -0400

    Made the generic object pane look less like the duke crane

--- src/gae/gameView/GenericObjectsPane.java
+++ src/gae/gameView/GenericObjectsPane.java
@@ -1,44 +1,47 @@
 package gae.gameView;
 
 import gae.editor.SimpleEditor;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class GenericObjectsPane {
 
-    private VBox baseNode;
+    private StackPane baseNode;
 
-    public GenericObjectsPane() {
+    public GenericObjectsPane () {
         initialize();
     }
-    
+
     private void initialize () {
-        baseNode = new VBox();
-        baseNode.setStyle("-fx-background-color: #FFFFFF;");
-        baseNode.setStyle("-fx-border-color: red;");
-        baseNode.setSpacing(10);
-        baseNode.setPadding(new Insets(15, 12, 15, 12));
+        ObservableList<String> data = FXCollections.observableArrayList();
 
-        // Make these classes?
-        Label towerLabel = makeNewClickableText("Tower", e -> newCustomObject("Tower"));
-        Label enemyLabel = makeNewClickableText("Enemy", e -> newCustomObject("Enemy"));
-        Label obstacleLabel = makeNewClickableText("Obstacle", e -> newCustomObject("Obstacle"));
-        baseNode.getChildren().addAll(new Label("Custom Object Types"), towerLabel, enemyLabel, obstacleLabel);
-    }  
+        ListView<String> listView = new ListView<String>(data);
+        listView.setPrefSize(200, 250);
+        listView.setEditable(true);
 
-    private Label makeNewClickableText(String title, EventHandler<MouseEvent> action) {
-        Label ret = new Label(title);
-        ret.setOnMouseClicked(action);
-        return ret;
+        data.addAll("Tower", "Enemy", "Obstacle"); // TODO: Don't hard code these values
+
+        listView.setItems(data);
+        listView.setOnMouseClicked(e -> listClicked(e, listView));
+
+        baseNode = new StackPane();
+        baseNode.getChildren().add(listView);
     }
-    
+
+    private void listClicked (MouseEvent click, ListView<String> list) {
+        if (click.getClickCount() == 2) {
+            newCustomObject(list.getSelectionModel().getSelectedItem());
+        }
+    }
+
     private void newCustomObject (String type) {
         SimpleEditor editor = new SimpleEditor();
         Scene editorScene = new Scene(new Pane(editor.getObject()));
@@ -50,5 +53,4 @@ public class GenericObjectsPane {
     public Node getBaseNode () {
         return baseNode;
     }
-
 }

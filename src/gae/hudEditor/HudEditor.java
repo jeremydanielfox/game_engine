2ccff71b93aa5b320347b20744124216176ac1ed
jgilhuly <jrg37@duke.edu>
2015-04-10 00:14:09 -0400

    fixed hud editor formatting, now resizable elements

--- src/gae/hudEditor/HudEditor.java
+++ src/gae/hudEditor/HudEditor.java
@@ -6,38 +6,53 @@ import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 
+
 public class HudEditor implements UIObject {
     private static final double ANCHOR_OFFSET = 5.0;
-    private static final double SCREEN_HEIGHT = Screen.getPrimary().getVisualBounds().getHeight();
-    private static final double SCREEN_WIDTH = Screen.getPrimary().getVisualBounds().getWidth();
+
+    private static final double SCREEN_HEIGHT =
+            Screen.getPrimary().getVisualBounds().getHeight() - 150;
+
     private List<EdittableHudLocation> locations;
     private List<Node> locationNodes;
     private AnchorPane rootNode;
+    private ImageView bImage;
 
-    public HudEditor() {
+    public HudEditor () {
+        initialize();
+    }
+
+    private void initialize () {
         rootNode = new AnchorPane();
+        rootNode.setMinHeight(SCREEN_HEIGHT);
+        
+        bImage = new ImageView();
+        bImage.fitWidthProperty().bind(rootNode.widthProperty());
+        bImage.fitHeightProperty().bind(rootNode.heightProperty());
+        rootNode.getChildren().add(bImage);
 
-        Button done = new Button ("Done");
+        Button done = new Button("Save HUD");
         done.setOnAction(e -> export());
         rootNode.getChildren().add(done);
 
+        setLocations();
+        locationNodes = extractNodesFromEHLList(locations);
+        rootNode.getChildren().addAll(locationNodes);
+
+        setAnchorPoints(done);
+    }
+
+    private void setLocations () {
         locations = new ArrayList<EdittableHudLocation>();
         locations.add(new EdittableHudLocation(Color.YELLOW));
         locations.add(new EdittableHudLocation(Color.GREEN));
         locations.add(new EdittableHudLocation(Color.RED));
         locations.add(new EdittableHudLocation(Color.BLUE));
-
-        locationNodes = extractNodesFromEHLList(locations);
-
-        rootNode.getChildren().addAll(locationNodes);
-
-        setAnchorPoints(done);
     }
 
     private void setAnchorPoints (Button done) {
@@ -53,8 +68,8 @@ public class HudEditor implements UIObject {
         AnchorPane.setRightAnchor(locationNodes.get(3), ANCHOR_OFFSET);
         AnchorPane.setBottomAnchor(locationNodes.get(3), ANCHOR_OFFSET);
 
-        AnchorPane.setBottomAnchor(done, SCREEN_HEIGHT/2);
-        AnchorPane.setLeftAnchor(done, SCREEN_WIDTH/2 - 50);
+        AnchorPane.setBottomAnchor(done, rootNode.getHeight() / 2);
+        AnchorPane.setLeftAnchor(done, rootNode.getWidth() / 2 - 50);
     }
 
     private List<Node> extractNodesFromEHLList (List<EdittableHudLocation> ehlList) {
@@ -81,8 +96,8 @@ public class HudEditor implements UIObject {
     public Node getObject () {
         return rootNode;
     }
-    
-    public void setBackgroundImage(Image backgroundImage) {
-        rootNode.setBackground(new Background(new BackgroundImage(backgroundImage, null, null, null, null)));
+
+    public void setBackgroundImage (Image backgroundImage) {
+        bImage.setImage(backgroundImage);
     }
 }

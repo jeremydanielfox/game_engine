a8c742407da7fab21648906f92be8f5f693fc007
Nathan Prabhu <nprab428@yahoo.com>
2015-04-10 02:18:24 -0400

    hacky method adding transition Tower to gameWorld

--- src/gameworld/BasicWorld.java
+++ src/gameworld/BasicWorld.java
@@ -24,21 +24,21 @@ public class BasicWorld implements GameWorld {
 
     public BasicWorld () {
         myObjects = new ArrayList<GameObject>();
-        myGrid=new GridFree(5,5);
+        myGrid = new GridFree(5, 5);
     }
 
     @Override
-    public void addObject (GameObject toSpawn, PointSimple pixelCoords){
+    public void addObject (GameObject toSpawn, PointSimple pixelCoords) {
         myObjects.add(toSpawn);
-        toSpawn.setPoint(pixelCoords);//TODO change from pixel coords
-        //myGrid.addObject(toSpawn);
+        toSpawn.setPoint(pixelCoords);// TODO change from pixel coords
+        // myGrid.addObject(toSpawn);
     }
 
     @Override
     public void updateGameObjects () {
-         for (GameObject o: myObjects){
-             o.update(this);
-         }
+        for (GameObject o : myObjects) {
+            o.update(this);
+        }
     }
 
     @Override
@@ -51,18 +51,18 @@ public class BasicWorld implements GameWorld {
     public void checkCollisions () {
         List<Buffable> buffables =
                 myObjects.stream().filter(p -> p.getClass().isAssignableFrom(Buffable.class))
-                .map(p -> (Buffable) p)
-                .collect(Collectors.toList());
+                        .map(p -> (Buffable) p)
+                        .collect(Collectors.toList());
         List<Buffer> buffers =
                 myObjects.stream().filter(p -> p.getClass().isAssignableFrom(Buffer.class))
-                .map(p -> (Buffer) p)
-                .collect(Collectors.toList());
+                        .map(p -> (Buffer) p)
+                        .collect(Collectors.toList());
         buffers.forEach(go1 -> {
             buffables.forEach(go2 -> {
                 if (go1.getGraphic().getNode().getBoundsInParent()
-                        .intersects(go2.getGraphic().getNode().getBoundsInParent())){
-                    //myCollisionEngine.interact(go1, go2);
-                    go1.collide(go2);
+                        .intersects(go2.getGraphic().getNode().getBoundsInParent())) {
+                    // myCollisionEngine.interact(go1, go2);
+                    //go1.collide(go2);
                 }
             });
         });
@@ -90,25 +90,22 @@ public class BasicWorld implements GameWorld {
     @Override
     public Collection<GameObject> objectsInRange (double range, PointSimple center) {
         ArrayList<GameObject> inRange = new ArrayList<>();
-        for (GameObject o : myObjects){
-            if (center.withinRange(o.getPoint(), range)){
+        for (GameObject o : myObjects) {
+            if (center.withinRange(o.getPoint(), range)) {
                 inRange.add(o);
             }
         }
         return Collections.unmodifiableList(inRange);
     }
 
-	@Override
-	public void addObject(GameObject toSpawn) {
-		myObjects.add(toSpawn);
-	}
+    @Override
+    public void addObject (GameObject toSpawn) {
+        myObjects.add(toSpawn);
+    }
 
-	@Override
-	public boolean isPlacable(GameObject toSpawn, PointSimple pixelCoords) {
-		return true; //TODO plz replace with logic. Ex: towers cannot be placed on towers
-	}
-
-
-    
+    @Override
+    public boolean isPlacable (GameObject toSpawn, PointSimple pixelCoords) {
+        return true; // TODO plz replace with logic. Ex: towers cannot be placed on towers
+    }
 
 }

65c8c926ef5051141dd8e33a9d564b9f91fe9010
Kaighn Kevlin <kaighn.kevlin@duke.edu>
2015-04-15 22:58:24 -0400

    changed isPlacable method to take node, not game object

--- src/gameworld/GameWorld.java
+++ src/gameworld/GameWorld.java
@@ -3,6 +3,7 @@ package gameworld;
 import java.util.Collection;
 import java.util.List;
 
+import javafx.scene.Node;
 import engine.gameobject.GameObject;
 import engine.gameobject.PointSimple;
 import engine.pathfinding.Path;
@@ -13,5 +14,5 @@ public interface GameWorld extends ObjectCollection {
 
 	public void addObject(GameObject toSpawn, PointSimple pixelCoords) throws StructurePlacementException;
 	
-	public boolean isPlacable(GameObject toSpawn, PointSimple pixelCoords);
+	public boolean isPlacable(Node n, PointSimple pixelCoords);
 }

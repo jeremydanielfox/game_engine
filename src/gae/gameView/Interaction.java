24c639f9096698b96801b99302a76f3fade59edf
Brandon Choi <jc440@duke.edu>
2015-04-17 02:40:31 -0400

    New images for future terrain, back end label tree, updates on interaction table

--- src/gae/gameView/Interaction.java
+++ src/gae/gameView/Interaction.java
@@ -5,17 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * Defines a single interaction between game objects
  * 
  * @author Brandon Choi
  *
+ */
+
+/*
+ * label1 [collide || no collide] label2
+ * -need access to library view for labels / objects
+ * -create tree of hierarchy for labels
  */
 
 public class Interaction {
@@ -26,12 +36,18 @@ public class Interaction {
 
     public Interaction () {
         container = new HBox();
-        interactionType = new DropDown("Interaction Type", Arrays.asList("1", "2", "3"));
+        interactionType = new DropDown("Interaction Type", Arrays.asList("Collide", "Do not collide"));
+        box1 = new ObjectContainer();
+        box2 = new ObjectContainer();
         createInteraction();
     }
 
     private void createInteraction () {
-
+        container.getChildren().addAll(box1.getContainer(), interactionType.getDropDown(), box2.getContainer());
+    }
+    
+    public Node getInteractionSetter() {
+        return container;
     }
 
     /**
@@ -48,6 +64,7 @@ public class Interaction {
 
         public DropDown (String n, List<String> options) {
             container = new VBox();
+            container.setId("interactionOptions");
             label = new Label(n);
             choices = new ComboBox();
             createDropDown(options);
@@ -72,17 +89,28 @@ public class Interaction {
     private class ObjectContainer {
         
         private VBox container;
+        private VBox selected;
         private ScrollPane scroller;
         private List<String> options;
+        private Button adder;
 
         public ObjectContainer() { 
             container = new VBox();
+            selected = new VBox();
+            container.setId("interactionBox");
             scroller = new ScrollPane();
             options = new ArrayList<>();
+            adder = new Button();
+            adder.setGraphic(new ImageView("/images/plus_sign.jpg"));
+            createObjectContainer();
         }
         
         private void createObjectContainer() {
-            
+            scroller.setContent(container);
+            adder.setOnMouseClicked(e -> {
+                
+            });
+            container.getChildren().addAll(selected, adder);
         }
         
         public Node getContainer(){
--- src/gae/gameView/InteractionTable.java
+++ src/gae/gameView/InteractionTable.java
@@ -1,6 +1,15 @@
 package gae.gameView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * table that will allow the user to define specific interactions between game objects such as what
@@ -10,18 +19,47 @@ import javafx.scene.control.Button;
  *
  */
 
-public class InteractionTable {
+public class InteractionTable extends Application{
 
     private static final String ADD_TEXT = "Add New Interaction";
+    
+    private BorderPane container;
+    private ScrollPane scroller;
+    private VBox content;
     private Button adder;
+    private List<Interaction> interactions;
 
     public InteractionTable () {
+        container = new BorderPane();
+        scroller = new ScrollPane();
+        container.setCenter(scroller);
         adder = new Button(ADD_TEXT);
+        container.setTop(adder);
+        interactions = new ArrayList<>();
+        content = new VBox();
+        scroller.setContent(content);
+        setUpButtons();
     }
 
     private void setUpButtons () {
         adder.setOnMouseClicked(e -> {
-            
+            Interaction i = new Interaction();
+            interactions.add(i);
+            content.getChildren().add(i.getInteractionSetter());
         });
+    }
+
+    
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage arg0) throws Exception {
+        Stage myStage = new Stage();
+        myStage.setWidth(Main.SCREEN_WIDTH);
+        myStage.setHeight(Main.SCREEN_HEIGHT);
+        myStage.setScene(new Scene(container));
+        myStage.show();
     }
 }

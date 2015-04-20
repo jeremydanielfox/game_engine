14d35053198b1c9805569f5a4d1bb8cd800fd259
Brandon Choi <jc440@duke.edu>
2015-04-20 00:09:45 -0400

    InteractionData complete and now holds all the data and setting methods

--- src/gae/gameView/InteractionData.java
+++ src/gae/gameView/InteractionData.java
@@ -1,8 +1,17 @@
 package gae.gameView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import engine.gameobject.GameObject;
+import engine.interactions.BuffImparter;
+import engine.interactions.CollisionEngine;
 import engine.interactions.Interaction;
+import engine.interactions.InteractionEngine;
+import engine.interactions.NoInteraction;
+import engine.interactions.RangeEngine;
+import engine.interactions.ShootAt;
 
 /**
  * Class that acts as the data holder of all interactions authored. This will hold the interactions
@@ -13,17 +22,65 @@ import engine.interactions.Interaction;
  */
 
 public class InteractionData {
-    
-    public List<Interaction> myInteractions;
- 
-    public InteractionData(){
-        
+
+    private List<InteractionEngine> myInteractionEngines;
+    private InteractionEngine myCollisions;
+    private InteractionEngine myShoots;
+
+    public InteractionData () {
+        myInteractionEngines = new ArrayList<>();
+        myCollisions = new CollisionEngine();
+        myShoots = new RangeEngine();
+        myInteractionEngines.addAll(Arrays.asList(myCollisions, myShoots));
     }
-    
+
     /**
-     * Called by the InteractionTable to add a new Interaction authored
+     * returns both interaction engines with respective interactions set in each of them
+     * 
+     * @return
      */
-    public void addInteraction(){
-        
+    public List<InteractionEngine> getEngines () {
+        return myInteractionEngines;
+    }
+
+    /**
+     * adds interactions based on one list of objects that interact in a certain way with another
+     * list of objects
+     * 
+     * @param one
+     * @param i
+     * @param two
+     */
+    public void addInteraction (List<GameObject> one, Interaction i, List<GameObject> two) {
+        if (i instanceof BuffImparter) {
+            massPut(one, i, two, myCollisions);
+        }
+        else if (i instanceof ShootAt) {
+            massPut(one, i, two, myShoots);
+        }
+        else {
+            massPut(one, new NoInteraction(), two, myCollisions);
+            massPut(one, new NoInteraction(), two, myShoots);
+        }
+    }
+
+    /**
+     * places all the interactions between list one and two by iterating through each of them and
+     * setting all iterations
+     * 
+     * @param one
+     * @param i
+     * @param two
+     * @param engine
+     */
+    private void massPut (List<GameObject> one,
+                          Interaction i,
+                          List<GameObject> two,
+                          InteractionEngine engine) {
+        one.forEach(e -> {
+            two.forEach(f -> {
+                engine.put(e.getLabel(), f.getLabel(), i);
+            });
+        });
     }
 }

a1efdd6d5c20318427e493bae5d5cb42367c8317
Brandon Choi <jc440@duke.edu>
2015-04-21 02:17:58 -0400

    Modifications to button creator in creating fields of different types

--- src/gae/gameView/ButtonCreator.java
+++ src/gae/gameView/ButtonCreator.java
@@ -10,13 +10,17 @@ import java.util.Map;
 import engine.fieldsetting.Settable;
 import engine.fieldsetting.Triggerable;
 import engine.game.StoryBoard;
+import engine.goals.Goal;
 import View.ButtonWrapper;
 import gae.editor.EditingParser;
 import gae.editor.Editor;
 import gae.openingView.OpeningView;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -40,29 +44,26 @@ public class ButtonCreator extends Application {
     private Map<Method, String> triggerableMap;
 
     public ButtonCreator () {
-        container = new VBox();
+        container = new VBox(15);
         settableMap = new HashMap<>();
         triggerableMap = new HashMap<>();
-        createMaps();
+        createMap(settableMap, ButtonWrapper.class, Settable.class);
+        createMap(triggerableMap, StoryBoard.class, Triggerable.class);
+        createFields();
+    }
+
+    public VBox getContainer () {
+        return container;
     }
 
     /**
      * fills settableMap and triggerableMap with the keys as the methods and the values as the
      * method property name
      */
-    private void createMaps () {
-        /*
-         * settableMap
-         */
-        parser.getMethodsWithAnnotation(ButtonWrapper.class, Settable.class).forEach(e -> {
-            settableMap.put(e, extractName(e.getName()));
-        });
-
-        /*
-         * triggerableMap
-         */
-        parser.getMethodsWithAnnotation(StoryBoard.class, Triggerable.class).forEach(e -> {
-            triggerableMap.put(e, extractName(e.getName()));
+    @SuppressWarnings({ "rawtypes", "static-access", "unchecked" })
+    private void createMap (Map m, Class klass, Class annotation) {
+        parser.getMethodsWithAnnotation(klass, annotation).forEach(e -> {
+            m.put(e, extractName(e.getName()));
         });
     }
 
@@ -74,19 +75,60 @@ public class ButtonCreator extends Application {
      */
     private String extractName (String method) {
         return method.substring(3);
+        /*
+         * how to split the capital letters with space?
+         */
     }
 
     /**
      * fills the creator with fields
      */
     private void createFields () {
-
+        settableMap.keySet().forEach(e -> {
+            createFieldSetter(settableMap.get(e), Arrays.asList(e.getParameterTypes()));
+        });
     }
 
-    private HBox createFieldContainer (String label) {
-        HBox field = new HBox(15);
+    /**
+     * creates the appropriate field setter based on the parameter type of the method that is passed
+     * in
+     */
+    @SuppressWarnings("rawtypes")
+    private void createFieldSetter (String methodName, List<Class> parameterTypes) {
+        HBox fieldSetter = new HBox(15);
+        Label label = new Label(methodName);
+        fieldSetter.getChildren().add(label);
+        
+        parameterTypes.forEach(e -> {
+            Node field = null;
+            
+            /*
+             * if field requires a String, then create a text field
+             */
+            if (e.isInstance(String.class)) {
+                field = new TextField();
+            }
 
-        return field;
+            /*
+             * if field requires a Consumer, then create drop down of all options for them to use
+             */
+            else if () {
+                
+            }
+
+            /*
+             * if field requires a Goal, then create a drop down of all goals created & give them
+             * option to create new Goal
+             */
+            else if () {
+
+            }
+            
+            fieldSetter.getChildren().add(field);
+        });
+        
+        
+        container.getChildren().add(fieldSetter);
     }
 
     public static void main (String[] args) {
@@ -96,7 +138,8 @@ public class ButtonCreator extends Application {
     @Override
     public void start (Stage arg0) throws Exception {
         Stage stage = new Stage();
-        Scene scene = new Scene(container);
+        ButtonCreator bc = new ButtonCreator();
+        Scene scene = new Scene(bc.getContainer(), 500, 500);
         stage.setScene(scene);
         stage.show();
     }

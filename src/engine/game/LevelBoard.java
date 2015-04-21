1848aed9d06363a2956b71aae1e4e6e987f2be6a
Sierra Smith <sms95@duke.edu>
2015-04-20 23:28:42 -0400

    Added more comments to LevelBoard interface

--- src/engine/game/LevelBoard.java
+++ src/engine/game/LevelBoard.java
@@ -14,27 +14,35 @@ import java.util.Observer;
 public interface LevelBoard {
 
     /**
-     * Updates everything necessary to switch to the next level.
+     * Updates everything necessary to switch to the next level. After this method is called,
+     * "update()" will begin updating the next level.
      */
     abstract void startNextLevel ();
 
     /**
-     * Returns a boolean that represents if the game is over or not.
+     * Returns a boolean that represents if the game is over or not as determined by the
+     * winning and losing conditions defined by the game.
      * 
      * @return
      */
     public boolean gameOver ();
 
+    /**
+     * Returns the string name of the image for the current level.
+     * 
+     * @return
+     */
     public String getCurrentLevelMap ();
 
     /**
-     * Updates all of the elements in the current level by one respective unit.
+     * Updates all of the elements in the current level by one frame.
      */
     public void update ();
 
     /**
-     * Adds an observer to this level board's collection of observers that are
-     * notified whenever a select change occurs in the level board.
+     * Adds an observer to this level board's collection of observers that will be
+     * notified whenever a change occurs in the level board that has been defined to notify
+     * observers.
      * 
      * @param o
      */
@@ -48,7 +56,7 @@ public interface LevelBoard {
     public GameWorld getGameWorld ();
 
     /**
-     * Returns the level object for the current level.
+     * Returns the level object that corresponds to the current level.
      * 
      * @return
      */

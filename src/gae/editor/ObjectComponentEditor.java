dff3b753bcfc6daa83329ddfeee640c882e0e4b5
Eric Saba <eric.saba@duke.edu>
2015-04-24 18:31:39 -0400

    checking that setImagePath on Editor still works... it doesgit add .

--- src/gae/editor/ObjectComponentEditor.java
+++ src/gae/editor/ObjectComponentEditor.java
@@ -49,7 +49,8 @@ public class ObjectComponentEditor extends ComponentEditor {
     public EventHandler<? super MouseEvent> popNewEditor() {
         return e -> {
             Consumer<Object> setObjectConsumer = o -> setObject(o);
-            new PopUpEditorView(setObjectConsumer, clazz);
+            GenericObjectsPane.newCustomObject(clazz, "yo", setObjectConsumer);
+//            new PopUpEditorView(setObjectConsumer, clazz);
         };
     }
 

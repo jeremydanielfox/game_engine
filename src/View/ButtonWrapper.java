cf1ad314af94076dbd4b324659f6052fe9197cd8
Sierra Smith <sms95@duke.edu>
2015-04-25 15:21:34 -0400

    Commented the Button Wrapper class

--- src/View/ButtonWrapper.java
+++ src/View/ButtonWrapper.java
@@ -8,8 +8,19 @@ import engine.goals.Goal;
 import engine.goals.NullGoal;
 
 
+/**
+ * This class contains information needed to create, enable, and set up the click action for a
+ * button. It was created to allow a game designer to add a custom button to the game and use a goal
+ * to determine when it should be enabled and give it a lambda function to execute on click.
+ * 
+ * @author Sierra
+ * @author Cosette
+ *
+ */
 @Settable
 public class ButtonWrapper {
+
+    private static final String DEFAULT_TEXT = "";
 
     @XStreamOmitField
     private transient Button myButton;
@@ -19,26 +30,40 @@ public class ButtonWrapper {
     private Goal myGoal;
 
     public ButtonWrapper () {
-        initializeFields("", e -> doNothing(), new NullGoal());
+        initializeFields(DEFAULT_TEXT, e -> doNothing(), new NullGoal());
     }
 
     public ButtonWrapper (String l, Consumer<? extends Object> action, Goal enable) {
-        myLabel = l;
-        myAction = action;
-        myGoal = enable;
+        initializeFields(l, action, enable);
     }
 
+    /**
+     * Takes in the information needed to construct a button and initializes everything.
+     * 
+     * @param label
+     * @param action
+     * @param enable
+     */
     private void initializeFields (String label, Consumer<? extends Object> action, Goal enable) {
         myButton = new Button(label);
         myButton.setOnAction(e -> action.accept(null));
         myEnableCondition = enable;
     }
 
+    /**
+     * This method is empty and is used as the default button action in the empty button wrapper
+     * constructor so that a button whose action is uninitialized will do nothing but will not cause
+     * a null pointer exception.
+     */
     private void doNothing () {
 
     }
 
-    // TODO: make initialize fields work
+    /**
+     * Initializes the button if it is currently null and returns the JavaFX button.
+     * 
+     * @return the actual JavaFX Button object
+     */
     public Button getButton () {
         if (myButton == null) {
             initializeFields(myLabel, myAction, myGoal);
@@ -46,6 +71,11 @@ public class ButtonWrapper {
         return myButton;
     }
 
+    /**
+     * Returns true is the enable condition (goal) is satisfied, otherwise false.
+     * 
+     * @return a boolean that indicates if the button should be enabled
+     */
     public boolean isEnabled () {
         if (myEnableCondition == null) {
             initializeFields(myLabel, myAction, myGoal);

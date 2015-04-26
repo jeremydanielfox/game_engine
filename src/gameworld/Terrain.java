1252e5f1bfe2505bc2b8f75f251b45ce4fb459cb
Kaighn Kevlin <kaighn.kevlin@duke.edu>
2015-04-26 15:40:09 -0400

    fixed terrain bug

--- src/gameworld/Terrain.java
+++ src/gameworld/Terrain.java
@@ -6,6 +6,7 @@ import engine.gameobject.PointSimple;
 public class Terrain {
 	private TerrainTile[][] myMatrix;
 	private CoordinateTransformer myTrans;
+	private GridCell myBounds;
 	
 	public Terrain(int numRows, int numCols, CoordinateTransformer t){
 		myMatrix = new TerrainTile[numRows][numCols];
@@ -15,6 +16,7 @@ public class Terrain {
 				myMatrix[i][j] = new TerrainTile();
 			}
 		}
+		myBounds = new GridCell(numRows,numCols);
 	}
 	
 	public boolean canPlaceStructure(PointSimple point){
@@ -37,7 +39,10 @@ public class Terrain {
 	public void setTerrain(TerrainTile t, int r, int c){
 		myMatrix[r][c] = t;
 	}
-	public TerrainTile getTerrainTile(GridCell c){
-		return myMatrix[c.getRow()-1][c.getCol()-1];
+	public TerrainTile getTerrainTile(GridCell c) throws InvalidArgumentException{
+		if(!c.withinBounds(myBounds)){
+			throw new InvalidArgumentException();
+		}
+		return myMatrix[c.getRow()][c.getCol()];
 	}
 }

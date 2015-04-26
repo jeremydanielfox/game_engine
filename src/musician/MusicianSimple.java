6215f2316e1cf6ec4ee2525deeebad7c2802f8cf
Brandon Choi <jc440@duke.edu>
2015-04-25 21:50:57 -0400

    Added preset sounds to MusicianSimple for games to use

--- src/musician/MusicianSimple.java
+++ src/musician/MusicianSimple.java
@@ -31,6 +31,8 @@ public class MusicianSimple implements Musician {
     private Map<Object, Music> myMusic;
     private boolean muted;
     private double savedVolume;
+    private Media rayGunSound, laserSound, gunSound;
+    private MediaPlayer rayGun, laser, gun;
 
     /*
      * try & catch blocks implemented for methods with MediaPlayer in case it isn't instantiated yet
@@ -41,6 +43,14 @@ public class MusicianSimple implements Musician {
         myMusic = new HashMap<>();
         muted = false;
         savedVolume = 0;
+        
+        rayGunSound = new Media("src/musician/raygun.wav");
+        laserSound = new Media("/src/musician/laser.wav");
+        gunSound = new Media("/src/musician/gun.wav");
+        
+        rayGun = new MediaPlayer(rayGunSound);
+        laser = new MediaPlayer(laserSound);
+        gun = new MediaPlayer(gunSound);
     }
 
     /**
@@ -132,5 +142,33 @@ public class MusicianSimple implements Musician {
         catch (Exception e) {
             /* does nothing if myMusician is not initialized */
         }
+    }
+
+    /**
+     * Preset sounds that can are also settable and thus customizable
+     */
+
+    public void rayGun () {
+        rayGun.play();
+    }
+
+    public void setRayGun (Music m) {
+        rayGunSound = m.getMusic();
+    }
+
+    public void laser () {
+        laser.play();
+    }
+
+    public void setLaser (Music m) {
+        laserSound = m.getMusic();
+    }
+
+    public void gun () {
+        gun.play();
+    }
+
+    public void setGun (Music m) {
+        gunSound = m.getMusic();
     }
 }

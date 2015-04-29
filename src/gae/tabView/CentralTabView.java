a84308ff16673742e9113a1de6ae9eb5a41492cc
Nina Sun <nina.sun@duke.edu>
2015-04-29 03:04:41 -0400

    fixed background image exporting to xml

--- src/gae/tabView/CentralTabView.java
+++ src/gae/tabView/CentralTabView.java
@@ -29,11 +29,14 @@ import java.util.function.Consumer;
 import voogasalad.util.pathsearch.graph.GridCell;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -171,7 +174,7 @@ public class CentralTabView implements UIObject {
         newTab.setContent(newLevel.getBaseNode());
         newTab.setClosable(false);
         tabView.getTabs().add(newTab);
-        ((HudEditorTab) hudTab).setBackgroundImage(levelView.getBackgroundImage());
+        ((HudEditorTab) hudTab).setBackgroundImage(new Image(levelView.getBackgroundImagePath().getValue()));
     }
 
     private WaveEditor createLevelAndWaveObject (GameWorld nextWorld, LevelPreferencesEditor prefs) {
@@ -208,6 +211,7 @@ public class CentralTabView implements UIObject {
         }
 
         prefs.setLevel(levelData);
+        levelView.setLevel(levelData);
         game.getLevelBoard().addLevel(levelData);
         return new WaveEditor(sb, levelData.getGameWorld());
     }
@@ -223,9 +227,6 @@ public class CentralTabView implements UIObject {
         }
         if (m.getName().equals("setGameWorld")) {
             m.invoke(levelData, nextWorld);
-        }
-        else if (m.getName().equals("setImagePath")) {
-            m.invoke(levelData, levelView.getBackgroundImagePath());
         }
     }
 
